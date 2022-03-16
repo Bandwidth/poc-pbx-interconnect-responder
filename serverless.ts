dbe52bc1ba12b79c6bafedbf02abdd2f2beef101
bw-mot <44877922+bw-mot@users.noreply.github.com>
2022-03-16 12:01:37 -0400

    Initial commit

---
+++ serverless.ts
@@ -0,0 +1,56 @@
+import type { AWS } from '@serverless/typescript';
+
+import hello from '@functions/hello';
+
+const BUCKET_NAME = "YOUR_BUCKET_NAME";
+const LAMBDA_SERVICE_NAME = "YOUR_LAMBDA_SERVICE_NAME";
+
+const serverlessConfiguration: AWS = {
+  // Name of your lambda service
+  service: LAMBDA_SERVICE_NAME,
+  frameworkVersion: '2',
+  custom: {
+    // Default stage for deployments will be dev
+    stage: '${opt:stage, "dev"}',
+    webpack: {
+      webpackConfig: './webpack.config.js',
+      includeModules: true,
+    },
+    client: {
+      bucketName: BUCKET_NAME + '-${self:custom.stage}',
+      indexDocument: 'index.html',
+      // TODO create your own error html!
+      // errorDocument: 'error.html',
+      distributionFolder: 'ui/build'
+    },
+  },
+  plugins: [
+    'serverless-webpack',
+    'serverless-dotenv-plugin',
+    'serverless-finch'
+  ],
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs14.x',
+    apiGateway: {
+      minimumCompressionSize: 1024,
+      shouldStartNameWithService: true,
+    },
+    environment: {
+      // Environment variables to be passed to your lambda
+      // should be defined here AND serverless.ts
+      // If you are testing deploying from your local machine,
+      // .env file needs to be defined at the project root.
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+      // Example of how to access environment params created via GHA
+      // TEST_PARAM: '${env:TEST_PARAM}',
+    },
+    lambdaHashingVersion: '20201221',
+  },
+  // import the function via paths
+  functions: {
+    hello
+  },
+};
+
+module.exports = serverlessConfiguration;
